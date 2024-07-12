08d7b60a84ef12d9f2cbaa73fc1ac35537bcc21d
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 21:48:30 +0800

    feat(Avatar): add custom size props

--- components/Avatar/index.js
+++ components/Avatar/index.js
@@ -3,9 +3,27 @@ import React from "react";
 
 import styles from "./styles";
 
-export default function Avatar() {
+export default function Avatar({ size }) {
+  function resolveAvatarSize() {
+    switch (size) {
+      case "sm":
+        return 35;
+      case "md":
+        return 48;
+      case "lg":
+        return 56;
+      default:
+        return 35;
+    }
+  }
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        { height: resolveAvatarSize(size), width: resolveAvatarSize(size) },
+      ]}
+    >
       <Text>B</Text>
     </View>
   );
--- components/Avatar/styles.js
+++ components/Avatar/styles.js
@@ -2,8 +2,6 @@ import { StyleSheet } from "react-native";
 
 export default styles = StyleSheet.create({
   container: {
-    width: 35,
-    height: 35,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
