3391fa0ad5f2170f4b89b1298616487eb13ae884
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 16:13:44 +0800

    feat(Avatar): add avatar component

---
+++ components/Avatar/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Avatar() {
+  return (
+    <View style={styles.avatar}>
+      <Text>B</Text>
+    </View>
+  );
+}
---
+++ components/Avatar/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  container: {
+    width: 35,
+    height: 35,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    backgroundColor: "#2dd4bf",
+  },
+});
