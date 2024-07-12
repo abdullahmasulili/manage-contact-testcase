973a7135afd7ea1e3d7429c0cd668c26105c23ca
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 16:41:05 +0800

    feat(ContactCard): add contact name property

--- components/ContactCard/index.js
+++ components/ContactCard/index.js
@@ -4,13 +4,13 @@ import React from "react";
 import styles from "./styles";
 import Avatar from "../Avatar";
 
-export default function ContactCard() {
+export default function ContactCard({ contactName }) {
   return (
     <View style={styles.container}>
       <Pressable android_ripple={{ color: "#f1f5f9" }}>
         <View style={styles.contentWrap}>
           <Avatar />
-          <Text>John Doe</Text>
+          <Text>{contactName}</Text>
         </View>
       </Pressable>
     </View>
