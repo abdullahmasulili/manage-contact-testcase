a2cfeaa4703ebe51a5185a959ad6201f576da965
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 14:22:28 +0800

    feat(screens): add contact list screen

---
+++ screens/ContactList/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function ContactList() {
+  return (
+    <View>
+      <Text>Contact List</Text>
+    </View>
+  );
+}
