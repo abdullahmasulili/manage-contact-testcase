efacf0ca93f922622c566907236738521a0d450b
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 21:49:09 +0800

    feat(contactSlicer): add get contact by id

--- redux/slicers/contactSlicer.js
+++ redux/slicers/contactSlicer.js
@@ -12,10 +12,22 @@ export const getContacts = createAsyncThunk(
   }
 );
 
+export const getContactById = createAsyncThunk(
+  "contact/fetchContactById",
+  async (contactId) => {
+    const response = await APIManager(`/contact/${contactId}`, {
+      method: "GET",
+    });
+
+    return response.data;
+  }
+);
+
 const contactSlice = createSlice({
   name: "contacts",
   initialState: {
     contacts: [],
+    contactDetails: {},
     loading: false,
     error: null,
   },
@@ -30,6 +42,17 @@ const contactSlice = createSlice({
         state.contacts = action.payload.data;
       })
       .addCase(getContacts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      })
+      .addCase(getContactById.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getContactById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.contactDetails = action.payload.data;
+      })
+      .addCase(getContactById.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       });
