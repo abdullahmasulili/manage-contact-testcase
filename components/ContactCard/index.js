92c78c719ac4cf9079e6433e58e7425c1f2df896
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 20:28:48 +0800

    feat(ContactCard): add full name conversion

--- components/ContactCard/index.js
+++ components/ContactCard/index.js
@@ -1,16 +1,32 @@
 import { View, Text, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import lodash from "lodash";
 
 import styles from "./styles";
 import Avatar from "../Avatar";
 
-export default function ContactCard({ contactName }) {
+export default function ContactCard({ firstName, lastName, contactId }) {
+  const [fullName, setFullName] = useState("");
+
+  function resolveFullName() {
+    const joinedName = firstName.concat(" ", lodash.upperFirst(lastName));
+    setFullName(lodash.upperFirst(joinedName));
+  }
+
+  function onCardPress() {
+    console.log("pressed");
+  }
+
+  useEffect(() => {
+    resolveFullName();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Pressable android_ripple={{ color: "#f1f5f9" }}>
+      <Pressable android_ripple={{ color: "#f1f5f9" }} onPress={onCardPress}>
         <View style={styles.contentWrap}>
           <Avatar />
-          <Text>{contactName}</Text>
+          <Text>{fullName}</Text>
         </View>
       </Pressable>
     </View>
