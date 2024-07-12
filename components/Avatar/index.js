89782cb933ce43956233e4bde5afc486fec537a3
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 16:16:08 +0800

    patch(Avatar): fix styling import

--- components/Avatar/index.js
+++ components/Avatar/index.js
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+import styles from "./styles";
+
 export default function Avatar() {
   return (
-    <View style={styles.avatar}>
+    <View style={styles.container}>
       <Text>B</Text>
     </View>
   );
