fb7804fdf70384a5db1fb66321ef936d69b99fef
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 19:55:09 +0800

    patch(App): add redux provider

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useEffect } from "react";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider, useDispatch } from "react-redux";
+
+import store from "./redux/store";
+
 import ContactList from "./screens/ContactList";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ContactList />
-    </View>
+    <Provider store={store}>
+      <SafeAreaProvider style={styles.container}>
+        <ContactList />
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
@@ -13,7 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingHorizontal: 20,
   },
 });
