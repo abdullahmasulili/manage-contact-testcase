0f1777a9c330f0a2c1b25b8a468b8dcc588da7be
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 16:14:22 +0800

    feat(components): add ContactCard component

---
+++ components/ContactCard/index.js
@@ -0,0 +1,17 @@
+import { View, Text, Pressable } from "react-native";
+import React from "react";
+
+import styles from "./styles";
+import Avatar from "../Avatar";
+
+export default function ContactCard() {
+  return (
+    <View style={styles.container}>
+      <Pressable android_ripple={{ color: "#e2e8f0" }}>
+        <View style={styles.contentWrap}>
+          <Avatar />
+        </View>
+      </Pressable>
+    </View>
+  );
+}
---
+++ components/ContactCard/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+  container: {
+    borderWidth: 1,
+    borderColor: "#e2e8f0",
+    borderRadius: 15,
+    overflow: "hidden",
+  },
+  avatar: {
+    width: 35,
+    height: 35,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 100,
+    backgroundColor: "#2dd4bf",
+  },
+  contentWrap: {
+    margin: 10,
+  },
+});
