01014bc8c5b5e8109f29c5aaa11f294eb8af51d1
Abdullah Masulili <abdullah.masulili@gmail.com>
2024-07-12 21:47:57 +0800

    feat(helpers): add GlobalStyles

---
+++ helpers/GlobalStyles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export default GlobalStyles = StyleSheet.create({
+  twoColumn: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 30,
+    marginVertical: 20,
+  },
+  textLight: {
+    fontWeight: "400",
+  },
+  textBold: {
+    fontWeight: "700",
+  },
+  textThick: {
+    fontWeight: "900",
+  },
+  textLarge: {
+    fontSize: 24,
+    marginVertical: 5,
+  },
+});
